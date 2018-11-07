aee42ddb8a27b97a06a9c5f2f4a528abd8149043
Charlie Splittstoser <charliesplittstoser@er-10-131-4-98.wireless.umn.edu>
2018-11-07 10:38:29 -0600

    Initial activity creation

---
+++ app/src/main/java/com/example/together/join_event.java
@@ -0,0 +1,14 @@
+package com.example.together;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class join_event extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_event);
+    }
+
+}
---
+++ app/src/main/java/com/example/together/photo_screen.java
@@ -0,0 +1,14 @@
+package com.example.together;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class photo_screen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_screen);
+    }
+
+}
