4e5fcc39cb031d4b40d0abfe937432d6a1a6a14c
Antonio Dimitrov <tonydimitrov97@gmail.com>
2018-11-11 16:47:40 -0600

    Added navigation from camera screen to the rest

--- app/src/main/java/com/example/together/EventListActivity.java
+++ app/src/main/java/com/example/together/EventListActivity.java
@@ -1,10 +1,16 @@
 package com.example.together;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.together.data.EventPreview;
 import com.example.together.data.EventPreviewAdapter;
@@ -20,10 +26,33 @@ public class EventListActivity extends AppCompatActivity {
     // Used to hold all the event previews
     RecyclerView mRecyclerView;
 
+    BottomNavigationView menu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_list);
+
+        // Setting listeners for the bottom navigation bar
+        menu = findViewById(R.id.eventListMenu);
+        menu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Intent intent;
+                switch(menuItem.getItemId()) {
+                    case R.id.createEvent:
+                        intent = new Intent(getApplicationContext(), CreateEventActivity.class);
+                        getApplicationContext().startActivity(intent);
+                        return true;
+                    case R.id.joinEvent:
+                        intent = new Intent (getApplicationContext(), JoinEventActivity.class);
+                        getApplicationContext().startActivity(intent);
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
 
         // Getting the RecyclerView from XML
         mRecyclerView = findViewById(R.id.previewRecyclerView);
