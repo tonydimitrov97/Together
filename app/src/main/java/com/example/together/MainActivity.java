9461eaa40f66b050af57c89626522113f7dc2a76
Antonio Dimitrov <tonydimitrov97@gmail.com>
2018-11-11 13:42:29 -0600

    Fixing issue which caused app to crash when camera is booted

--- app/src/main/java/com/example/together/MainActivity.java
+++ app/src/main/java/com/example/together/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.together;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.google.android.cameraview.CameraView;
 
 
 public class MainActivity extends AppCompatActivity {
-    CameraView mCameraView;
+    CameraView mCameraView = null;
+    private static final int REQUEST_CAMERA_PERMISSION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +23,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        mCameraView.start();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED) {
+            mCameraView.start();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                    REQUEST_CAMERA_PERMISSION);
+        }
     }
 
     @Override
     protected void onPause() {
-        mCameraView.stop();
+        if (mCameraView != null) {
+            mCameraView.stop();
+        }
         super.onPause();
     }
 }
