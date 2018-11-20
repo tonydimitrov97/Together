02af991c8a06c210bbeb49498689c0b95e32fc0c
Antonio Dimitrov <tonydimitrov97@gmail.com>
2018-11-19 18:38:39 -0600

    Editing event settings page so it looks like the other one

--- app/src/main/java/com/example/together/configuration/Configuration.java
+++ app/src/main/java/com/example/together/configuration/Configuration.java
@@ -2,11 +2,11 @@ package com.example.together.configuration;
 
 public class Configuration {
 
-    public static final String SERVER_IP = "http://10.131.66.19:3000/";
+    //public static final String SERVER_IP = "http://10.131.66.19:3000/";
 
-    public static final String UPLOAD_IP = "http://10.131.66.19:8888/upload.php";
+    public static final String UPLOAD_IP = "http://10.131.18.187:8888/upload.php";
 
-    //public static final String SERVER_IP = "http://10.131.100.15:3000/"; //Tony School
+    public static final String SERVER_IP = "http://10.131.18.187:3000/"; //Tony School
 
     //public static final String SERVER_IP = "http://10.131.204.60:3000/"; //Charlie School
 
