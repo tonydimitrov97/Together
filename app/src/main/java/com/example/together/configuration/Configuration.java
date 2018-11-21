28bc0f77f4ba5430c969c730793cc82baf153ba4
CharlieSplittstoser <char.split@gmail.com>
2018-11-21 00:50:01 -0600

    change ip

--- app/src/main/java/com/example/together/configuration/Configuration.java
+++ app/src/main/java/com/example/together/configuration/Configuration.java
@@ -4,9 +4,9 @@ public class Configuration {
 
     //public static final String SERVER_IP = "http://10.131.66.19:3000/";
 
-    public static final String UPLOAD_IP = "http://10.131.18.187:8888/upload.php";
+    public static final String UPLOAD_IP = "http://10.131.66.19:8888/upload.php";
 
-    public static final String SERVER_IP = "http://10.131.18.187:3000/"; //Tony School
+    public static final String SERVER_IP = "http://10.131.66.19:3000/"; //Tony School
 
     //public static final String SERVER_IP = "http://10.131.204.60:3000/"; //Charlie School
 
