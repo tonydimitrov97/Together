d6248bb3fecaeaf09c304efe21d449e6d4e2e6a0
Antonio Dimitrov <tonydimitrov97@gmail.com>
2018-11-20 18:14:37 -0600

    Made event id and privacy icons appear on event list screen

--- app/src/main/java/com/example/together/EventListActivity.java
+++ app/src/main/java/com/example/together/EventListActivity.java
@@ -10,7 +10,6 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
